a386db22516da980ff27541b415c5a0674d6457a
Mason Pott <masoncpott@gmail.com>
2021-08-27 15:02:08 -0700

    basics

--- basics.py
+++ basics.py
@@ -1,1 +1,6 @@
+#declare integer variables
+dayHours = 24
+daysPerWeek = 7
 
+hoursPerWeek = dayHours * daysPerWeek
+print(hoursPerWeek)
---
+++ basicsOld.py
@@ -0,0 +1,2 @@
+# print out the number of hours in a week
+print(24 * 7) #prints 168
