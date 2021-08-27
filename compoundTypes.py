405e250afc5d2cf84b8821fcd6aece4a58a95a8a
Mason Pott <masoncpott@gmail.com>
2021-08-27 16:54:04 -0700

    compound types, calculated avg

--- compoundTypes.py
+++ compoundTypes.py
@@ -1,4 +1,4 @@
-# list
+# List
 # you can store multiple objects in a list
 
 student_grades = [9.1, 8.8, 7.5]
@@ -8,3 +8,15 @@ student_grades = [9.1, 8.8, 7.5]
 example_list = list(range(1, 10))
 second_list = list(range(1, 10, 2)) # [1, 3, 5, 7, 9]
 print(example_list)
+
+# Attributes
+dir(list) # will print all the methods and attributes you can do on a list. Also works for any datatype
+
+dir(__builtins__) # displays all built in functions
+
+sum_of_grades = sum(student_grades)
+count_of_grades = len(student_grades)
+
+average_of_grades = sum_of_grades / count_of_grades
+
+print(average_of_grades)
