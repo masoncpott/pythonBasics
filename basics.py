2544d99953db2ee061caf0d041262e23fa72b8c2
Mason Pott <masoncpott@gmail.com>
2021-08-27 15:06:12 -0700

    more basics

--- basics.py
+++ basics.py
@@ -3,4 +3,10 @@ dayHours = 24
 daysPerWeek = 7
 
 hoursPerWeek = dayHours * daysPerWeek
-print(hoursPerWeek)
+print(hoursPerWeek) # prints 168
+
+x = 13
+y = 2
+z = "testing"
+
+print(x, y, z) # prints 13 2 testing
