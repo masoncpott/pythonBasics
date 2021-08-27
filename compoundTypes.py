429cb8c7c4d82599213ad897e14da28ec288aea7
Mason Pott <masoncpott@gmail.com>
2021-08-27 16:33:15 -0700

    compoundTypes

---
+++ compoundTypes.py
@@ -0,0 +1,10 @@
+# list
+# you can store multiple objects in a list
+
+student_grades = [9.1, 8.8, 7.5]
+
+# using "range", you can create a list of numbers automatically
+# the first two arguments are the range (inclusive of the first and noninclusive of the last), a third parameter can be provided and it is the step.
+example_list = list(range(1, 10))
+second_list = list(range(1, 10, 2)) # [1, 3, 5, 7, 9]
+print(example_list)
