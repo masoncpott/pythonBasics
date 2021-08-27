f0404aabf62ffbd8242656cfd06c2cf6ec317dd0
Mason Pott <masoncpott@gmail.com>
2021-08-27 16:25:45 -0700

    simpleTypes

---
+++ simpleTypes.py
@@ -0,0 +1,9 @@
+x = 10 # type integer
+y = '10' # type string, could also use double quotes
+z = 10.1 # type float
+
+sum1 = x + x # 20
+sum2 = y + y # 1010
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
