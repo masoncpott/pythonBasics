c1c6d5f8ff5dc0f47e7f2ac777796e34ed35cdaf
Mason Pott <masoncpott@gmail.com>
2021-08-29 20:04:39 -0700

    multiple string variables

--- stringFormatting.py
+++ stringFormatting.py
@@ -5,3 +5,17 @@ message = "Hello %s!" % user_input #construct the message. the '%s' will be repl
 alternate_message = f"Hello {user_input}!"
 print("Message:", message)
 print("Alternate version: ", alternate_message)
+
+
+
+
+# String Formatting with Multiple Variables
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+when = "today"
+
+name_message = "Hello %s %s!" % (first_name, last_name)
+alternate_name_message = f"Hello {first_name} {last_name}! What is up {when}?"
+
+print("Name Message: ", name_message)
+print("Alternate Name Message: ", alternate_name_message)
