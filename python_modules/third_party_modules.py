45dae8fc0002ca56aab1da7b36005ef2c2d1fa4d
Mason Pott <mason.pott@slalom.com>
2022-06-05 17:54:57 -0700

    jupyter

--- python_modules/third_party_modules.py
+++ python_modules/third_party_modules.py
@@ -3,11 +3,11 @@ import os
 import pandas
 
 while True:
-    if os.path.exists("temps_today.csv"):
+    if os.path.exists("/temps_today.csv"):
         data = pandas.read_csv("temps_today.csv") # type(data) = <class 'pandas.core.frame.DataFrame'>
         print(data.mean())
     else:
-        print("file does not exists")
+        print("file does not exist")
     time.sleep(4)
 
 #install pandas with pip3.9
